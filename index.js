5234f75e13b9718e761cad18386b56b510481381
wang zuo <wzuoadjusted@gmail.com>
2015-06-24 16:46:46 +0800

    fix history2.init redirecting with querystring

--- index.js
+++ index.js
@@ -1,51 +1,46 @@
 var assign = require('object-assign');
 require('./lib/starts-with');
 var PubSub = require('./lib/pub-sub');
+var location = window.location;
+var history = window.history;
 
 var _change = true; // hashchange event handler lock
 window._HISTORY_POPPED = false;
 var initialURL = location.href;
-var pushStateSupport = window.history && window.history.pushState;
+var pushStateSupport = history && history.pushState;
 
 function init(options) {
   var mode = this.mode = options.mode || (pushStateSupport ? 'pushstate' : 'hashbang');
   var basePath = this.basePath = options.basePath || '/';
 
-  var path = null;
-  var pathname = window.location.pathname;
-  var hash = window.location.hash;
+  var url = null;
+  var pathname = location.pathname;
+  var originalUrl = pathname + location.search;
+  var hash = location.hash;
   var start = hash.startsWith('#!');
 
   if(start) {
-    path = hash.substring(2);
-    if(mode === 'pushstate') return window.location.replace(path);
+    url = hash.substring(2);
+    if(mode === 'pushstate') return location.replace(path);
   } else if(hash) {
-    if(mode === 'pushstate') {
-      path = '/' + hash.substring(1).replace(/-/g, '/');
-      // return window.location.replace(url);
-    } else if(mode === 'hashbang') {
-      path = '/#!' + hash.substring(1).replace(/-/g, '/');
-      return window.location.replace(url);
-    }
-  } else {
-    path = pathname + window.location.search;
-    if(pathname !== '/' && mode === 'hashbang') {
-      return window.location.replace('/#!/'+pathname.substring(1));
-    }
+    var _url = hash.substring(1).replace(/-/g, '/'); // todo: convert url
+    if(mode === 'pushstate') url = '/' + _url;
+    else if(mode === 'hashbang') return location.replace('/#!' + _url);
+  } else if(pathname !== '/' && mode === 'hashbang') {
+    return location.replace('/#!/'+pathname.substring(1)+location.search);
   }
 
   this.start();
-
-  return path;
+  return url;
 }
 
 function change(url) {
   window._HISTORY_POPPED = true;
 
   if(this.mode === 'pushstate') {
-    window.history.pushState({url: url}, url, url);
+    history.pushState({url: url}, url, url);
   } else if(this.mode === 'hashbang') {
-    window.location.hash = '#!' + url;
+    location.hash = '#!' + url;
     _change = false; // disable hashchange event handler
   }
 };
@@ -53,9 +48,9 @@ function change(url) {
 // not working
 function replace(url) {
   if(this.mode === 'pushstate') {
-    window.history.replaceState({url: url}, url, url);
+    history.replaceState({url: url}, url, url);
   } else if(this.mode === 'hashbang') {
-    window.location.replace(url);
+    location.replace(url);
     _change = false;
   }
 }
