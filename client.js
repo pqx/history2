2d094fa552d0b07bc972b740311a517ba3b1d240
wang zuo <wzuoadjusted@gmail.com>
2015-06-25 18:46:41 +0800

    rebuild client.js

--- client.js
+++ client.js
@@ -2,51 +2,45 @@
 var assign = require('object-assign');
 require('./lib/starts-with');
 var PubSub = require('./lib/pub-sub');
+var location = window.location;
+var history = window.history;
 
 var _change = true; // hashchange event handler lock
 window._HISTORY_POPPED = false;
 var initialURL = location.href;
-var pushStateSupport = window.history && window.history.pushState;
+var pushStateSupport = history && history.pushState;
 
 function init(options) {
   var mode = this.mode = options.mode || (pushStateSupport ? 'pushstate' : 'hashbang');
   var basePath = this.basePath = options.basePath || '/';
 
-  var path = null;
-  var pathname = window.location.pathname;
-  var hash = window.location.hash;
-  var start = hash.startsWith('#!');
+  var url = null;
+  var pathname = location.pathname;
+  var originalUrl = pathname + location.search;
+  var hash = location.hash;
+  var start = hash && hash.startsWith('#!');
 
-  if(start) {
-    path = hash.substring(2);
-    if(mode === 'pushstate') return window.location.replace(path);
-  } else if(hash) {
-    if(mode === 'pushstate') {
-      path = '/' + hash.substring(1).replace(/-/g, '/');
-      // return window.location.replace(url);
-    } else if(mode === 'hashbang') {
-      path = '/#!' + hash.substring(1).replace(/-/g, '/');
-      return window.location.replace(url);
-    }
-  } else {
-    path = pathname + window.location.search;
-    if(pathname !== '/' && mode === 'hashbang') {
-      return window.location.replace('/#!/'+pathname.substring(1));
-    }
+  if(mode === 'pushstate') {
+    if(start) return location.replace(hash.substring(2));
+    url = originalUrl;
+  }
+
+  if(mode === 'hashbang') {
+    if(!start) return location.replace('/#!' + originalUrl);
+    url = hash.substring(2);
   }
 
   this.start();
-
-  return path;
+  return url;
 }
 
 function change(url) {
   window._HISTORY_POPPED = true;
 
   if(this.mode === 'pushstate') {
-    window.history.pushState({url: url}, url, url);
+    history.pushState({url: url}, url, url);
   } else if(this.mode === 'hashbang') {
-    window.location.hash = '#!' + url;
+    location.hash = '#!' + url;
     _change = false; // disable hashchange event handler
   }
 };
@@ -54,9 +48,9 @@ function change(url) {
 // not working
 function replace(url) {
   if(this.mode === 'pushstate') {
-    window.history.replaceState({url: url}, url, url);
+    history.replaceState({url: url}, url, url);
   } else if(this.mode === 'hashbang') {
-    window.location.replace(url);
+    location.replace(url);
     _change = false;
   }
 }
